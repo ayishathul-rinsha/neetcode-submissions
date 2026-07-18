7bd1b1cb7f4af68500243f3d601f9929a61d565f
Ayishathul Rinsha P <ayisharinshapadickal@gmail.com>
2026-07-18 12:09:41 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        r=[]
+        for i in strs:
+            r.append(f'{len(i)}')
+            r.append('|')
+            r.append(i)
+
+        return ''.join(r)
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i=0
+        while i<len(s):
+            j=i
+            while s[j]!='|':
+                j+=1
+            l=int(s[i:j])
+            res.append(s[j+1:j+1+l])
+            i=j+1+l
+
+        return res
